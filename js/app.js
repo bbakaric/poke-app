4116e9c3f600dc7641d720eda2e52fba29985cb6
Bruno Bakarić <bak.13@hotmail.com>
2021-05-25 08:25:20 +0200

    Commit 2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const btn = document.getElementById('menu');
+const menu = document.querySelector('ul');
 
-const show = () => {
-    alert('test');
+const addShow = () => {
+    menu.classList.add('show');
 };
 
-btn.addEventListener('click', show);
+const removeShow = () => {
+    menu.classList.remove('show');
+};
+
+btn.addEventListener('click', addShow);
+menu.addEventListener('pointerleave', removeShow);
