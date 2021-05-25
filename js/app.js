2199d09101a3802a143d8d7ead796023262c07e9
Bruno Bakarić <bak.13@hotmail.com>
2021-05-25 12:15:17 +0200

    Commit after reorganizing documents

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 const btn = document.getElementById('menu');
 const menu = document.querySelector('ul');
+const login = document.getElementById('loginBtn');
 
 const addShow = () => {
-    menu.classList.add('show');
-};
-
-const removeShow = () => {
-    menu.classList.remove('show');
+    menu.classList.toggle('show');
 };
 
 btn.addEventListener('click', addShow);
-menu.addEventListener('pointerleave', removeShow);
