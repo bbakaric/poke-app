e4f60fbaf4db70fab0256b5c571e5ef58b89ffe3
Bruno Bakarić <bak.13@hotmail.com>
2021-05-27 09:55:57 +0200

    New plan

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 const btn = document.getElementById('menu');
 const menu = document.querySelector('ul');
-const login = document.getElementById('loginBtn');
 
 const addShow = () => {
     menu.classList.toggle('show');
 };
 
 btn.addEventListener('click', addShow);
+const loginBtn = document.getElementById('loginBtn');
+const loginBack = document.getElementById('login-back');
+const username = document.getElementById('username');
+
+const checkInput = () => {
+    let user = username.value.trim();
+    if(user !== "") {
+        localStorage.setItem("username", user);
+        loginBack.classList.add('login-back-hidden');
+        console.log(localStorage);
+    } else {
+        alert("Enter username!");
+    }
+};
+
+loginBtn.addEventListener('click', checkInput);
