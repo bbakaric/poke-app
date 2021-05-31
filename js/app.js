9575afdd0f4f8e55b44477dfc1a506bd0340b705
Bruno Bakarić <bak.13@hotmail.com>
2021-05-31 19:43:08 +0200

    GET

--- js/app.js
+++ js/app.js
@@ -1,24 +1,49 @@
-const btn = document.getElementById('menu');
-const menu = document.querySelector('ul');
+const searchBar = document.getElementById('searchBar');
+const searchBtn = document.getElementById('searchBtn');
+const randomBtn = document.getElementById('randomBtn');
+const infoContainer = document.getElementById('infoContainer');
 
-const addShow = () => {
-    menu.classList.toggle('show');
-};
-
-btn.addEventListener('click', addShow);
-const loginBtn = document.getElementById('loginBtn');
-const loginBack = document.getElementById('login-back');
-const username = document.getElementById('username');
-
-const checkInput = () => {
-    let user = username.value.trim();
-    if(user !== "") {
-        localStorage.setItem("username", user);
-        loginBack.classList.add('login-back-hidden');
-        console.log(localStorage);
-    } else {
-        alert("Enter username!");
+searchBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    let inputData = searchBar.value.trim();
+    if(inputData !== ""){
+        let xhr;
+        if(window.XMLHttpRequest){
+            xhr = new XMLHttpRequest();
+        }
+        else{
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
     }
-};
 
-loginBtn.addEventListener('click', checkInput);
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let pokeInfo = "";
+            let pokeData = JSON.parse(this.responseText);
+            if(pokeData.length !== 0){
+                let image, name, type, hp, attack, defense;
+                for(const poke in pokeData){
+                    // image = pokeData[poke].sprites.other[1].front_default;
+                    name = pokeData[poke].name;
+                    // type = pokeData[poke].types.type.name;
+                    // hp = pokeData[poke].stats[0].base_stat;
+                    // attack = pokeData[poke].stats[1].base_stat;
+                    // defense = pokeData[poke].stats[2].base_stat;
+
+                    pokeInfo += `<ul><li>${name}</li></ul>`;
+                }
+            }
+            else{
+                pokeInfo = 'Please enter valid Pokemon name!';
+            }
+            infoContainer.innerHTML = pokeInfo;
+        }
+    }
+    xhr.open("GET", `https://pokeapi.co/api/v2/pokemon/${inputData}`, true);
+    xhr.send();
+    }
+    else{
+        while(infoContainer.hasChildNodes()){
+            infoContainer.removeChild(infoContainer.firstChild);
+        }
+    }
+});
